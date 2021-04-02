73e300af0299acea2639c7dbd693e2a7b5369a62
Alec McNamara <alecgmcnamara@gmail.com>
2021-04-02 15:46:29 +0100

    Tidy Canvas Element

--- data/script.js
+++ data/script.js
@@ -1,69 +1,57 @@
 const canvas = document.getElementById("Axis1Canvas");
+const ctx = canvas.getContext("2d");
+const gridborder = 25;
+const gridspacing = 50;
 
-const border = 25;
+window.addEventListener('load', onload);
+function onload(event) {
+    DrawLegend("10ms / Div","1.0v / Div");
+}
+setInterval(draw);
 
-function draw(color){
-   // var c = document.getElementById("Axis1Canvas");
-    var ctx = canvas.getContext("2d");
-
-    ctx.clearRect(border, border, canvas.width - (border * 2), canvas.height - (border*2));
+function draw(color)
+{
+    ctx.clearRect(gridborder, gridborder, canvas.width - 
+        (gridborder * 2), canvas.height - (gridborder*2));
 
     //draw graph lines
     ctx.beginPath();
     ctx.lineWidth = "1";
     ctx.strokeStyle = "lightgray";
-
-    for(gl = 50 + border;gl<400 - border ;gl += 50)
+    //horizontal
+    for(gl = gridspacing + gridborder;gl<canvas.height - gridborder ;gl += gridspacing)
     {
-        ctx.moveTo(border,gl);
-        ctx.lineTo(600-border,gl);
+        ctx.moveTo(gridborder,gl);
+        ctx.lineTo(canvas.width -gridborder,gl);
     }
-    for(gl = 50 + border;gl<600 - border;gl += 50)
+    //vertical
+    for(gl = gridspacing + gridborder;gl<canvas.width - gridborder;gl += gridspacing)
     {
-        ctx.moveTo(gl,border);
-        ctx.lineTo(gl,400 - border);
+        ctx.moveTo(gl,gridborder);
+        ctx.lineTo(gl,canvas.height - gridborder);
     }
-
-
     ctx.stroke();
 
     // draw readings
     var myrnd = + Math.random() * 2
-    
     ctx.beginPath();
     ctx.lineWidth = "2";
     ctx.strokeStyle = "green";
 
-    ctx.moveTo(border + myrnd + 2, 300 + myrnd);
+    ctx.moveTo(gridborder + myrnd + 2, 300 + myrnd);
     for(loop = 1;loop < 11;loop++) {
-
         ctx.lineTo(loop * 55 + myrnd, 300 + (loop % 2 * -200) + myrnd);
-
-    }
-     
+    }     
     ctx.stroke(); // Draw it
-    
 }
-function DrawLegend()
+function DrawLegend(hText,vText)
 {
-    var ctx = canvas.getContext("2d");
-    ctx.font = "14px Arial";
-
-    ctx.fillText("5.0", 5, 75);
-    ctx.fillText("4.0", 5, 125);
-    ctx.fillText("3.0", 5, 175);
-    ctx.fillText("2.0", 5, 225);
-    ctx.fillText("1.0", 5, 275);
-    ctx.fillText("0.0", 5, 325);
-
-    ctx.fillText("10mS / div", 280, 395);
-    
-
+    ctx.font = "16px Arial";
+    ctx.fillText(hText, (canvas.width - ctx.measureText(hText).width) / 2, canvas.height -5);
+    ctx.save();
+    ctx.translate( canvas.width - 1, 0 );
+    ctx.rotate( 3 * Math.PI / 2 );
+    ctx.fillText(vText, (canvas.height + ctx.measureText(vText).width) / 2 * -1 , 15 - canvas.width );
+    ctx.restore();
 }
     
-window.addEventListener('load', onload);
-function onload(event) {
-    DrawLegend();
-}
-
-setInterval(draw);
