a88ee80ce925331f7f92b8f78460679efc0b0e11
Alec McNamara <alecgmcnamara@gmail.com>
2021-04-02 20:05:51 +0100

    Draw graph from json variable

--- data/script.js
+++ data/script.js
@@ -2,12 +2,36 @@ const canvas = document.getElementById("Axis1Canvas");
 const ctx = canvas.getContext("2d");
 const gridborder = 25;
 const gridspacing = 50;
+const yaxisRef = 325;
+const xaxisMultiplier = 50;
+const yaxisMultiplier = 5;
+
+var JSONReceived =
+    
+        {axis1 : [
+            {"time": 0 ,"reading": 0} ,
+            {"time": 8,"reading": 5} ,
+            {"time": 20 ,"reading": 0} ,
+            {"time": 35 ,"reading": 5} ,
+            {"time": 52 ,"reading": 0} ,
+            {"time": 62 ,"reading": 5} ,
+            {"time": 78 ,"reading": 0} ,
+            {"time": 85 ,"reading": 5} ,
+            {"time": 110 ,"reading": 0 } ]
+        };
 
 window.addEventListener('load', onload);
 function onload(event) {
     DrawLegend("10ms / Div","1.0v / Div");
+
+    for(dbg=0 ;dbg<JSONReceived.axis1.length; dbg++)
+    {
+        console.log(JSONReceived.axis1[dbg].time);
+        console.log(JSONReceived.axis1[dbg].reading);    
+    }
+    console.log(JSONReceived.length);
 }
-setInterval(draw);
+setInterval(draw,100); //100msec test
 
 function draw(color)
 {
@@ -33,15 +57,22 @@ function draw(color)
     ctx.stroke();
 
     // draw readings
-    var myrnd = + Math.random() * 2
     ctx.beginPath();
     ctx.lineWidth = "2";
     ctx.strokeStyle = "green";
 
-    ctx.moveTo(gridborder + myrnd + 2, 300 + myrnd);
-    for(loop = 1;loop < 11;loop++) {
-        ctx.lineTo(loop * 55 + myrnd, 300 + (loop % 2 * -200) + myrnd);
-    }     
+    //startposition
+    ctx.moveTo(JSONReceived.axis1[0].time * yaxisMultiplier + gridborder,
+        yaxisRef - JSONReceived.axis1[0].reading * xaxisMultiplier);
+    for(drw=1 ;drw<JSONReceived.axis1.length; drw++)
+    {
+        ctx.lineTo(JSONReceived.axis1[drw].time * yaxisMultiplier + gridborder,
+            yaxisRef - JSONReceived.axis1[drw-1].reading * xaxisMultiplier);
+
+        ctx.lineTo(JSONReceived.axis1[drw].time * yaxisMultiplier + gridborder, 
+            yaxisRef - JSONReceived.axis1[drw].reading * xaxisMultiplier);   
+    }
+         
     ctx.stroke(); // Draw it
 }
 function DrawLegend(hText,vText)
