c3042ba34009c4ef1053c2af3f3e5dd9c3a8ff6a
Alec McNamara <alecgmcnamara@gmail.com>
2021-04-04 19:54:39 +0100

    test

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,46 @@
 #include <Arduino.h>
+#include <ArduinoJSON.h>
 
-void setup() {
-  // put your setup code here, to run once:
+/*Board connections
+  esp12e   Encoder
+  3v3      Vcc
+  Gnd      Gnd
+  D2       CLK
+  D3       DT
+  Tested on D0,D1 and D6,D7 
+  All GPIO can be used except D16  */
+
+#define CLK D2
+//volatile int intCount = MIN_COUNT;
+volatile bool Triggered = false;
+unsigned long TriggerTime = 0;
+StaticJsonDocument <200> jsonWrite;
+
+void ICACHE_RAM_ATTR myISR()
+{
+  cli(); //disable interupts
+  if(!Triggered){
+    Triggered = true;
+    TriggerTime = millis();
+  }
+
+  sei(); //enable interupts
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void setup()
+{
+  Serial.begin(115200);
+  Serial.println();
+  pinMode(CLK, INPUT);
+  attachInterrupt(digitalPinToInterrupt(CLK), myISR, CHANGE);
+}
+
+void loop()
+{
+  if(Triggered && TriggerTime+100 < millis()){
+    Triggered = false;
+    Serial.println("Finished");
+  }  
+
+
 }
